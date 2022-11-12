045b790205c0a486d19ec53b28d69ff3c7796cf1
BeatLink <beatlink+git@simplelogin.co>
2022-11-12 18:52:34 -0500

    Add main folder

---
+++ task_center/__main__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+This is the main entrypoint for the application and where the GTK GUI is launched from
+"""
+
+# Imports ##############################################################################################################
+from task_center.ui.gtk import run_app
+
+# Start App ############################################################################################################
+if __name__ == '__main__':
+    run_app()
