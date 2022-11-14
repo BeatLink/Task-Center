f558d3aab8af0307a712d2d6381ef66422391d5b
BeatLink <beatlink+git@simplelogin.co>
2022-11-14 12:03:02 -0500

    Add Settings

---
+++ task_center/core/settings/settings.py
@@ -0,0 +1,23 @@
+import pathlib
+import json
+
+
+class Settings:
+    def __init__(self, appinfo):
+        # Setup Settings File
+        self.settings_folder = pathlib.Path(appinfo.application_folders.user_config_dir)
+        self.settings_folder.mkdir(parents=True, exist_ok=True)
+        self.settings_file = self.settings_folder / "settings.json"
+
+        # The settings itself
+        self.settings_dict = {}
+
+    def load(self):
+        if not self.settings_file.exists():
+            self.save()
+        with self.settings_file.open("r") as settings_file:
+            self.settings_dict = json.load(settings_file)
+
+    def save(self):
+        with self.settings_file.open("w") as settings_file:
+            json.dump(self.settings_dict, settings_file, indent=4)
