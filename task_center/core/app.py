83183909a9035d1bdfe8c338adb83e983237740f
BeatLink <beatlink+git@simplelogin.co>
2022-12-23 21:54:36 -0500

    Update Core

--- task_center/core/app.py
+++ task_center/core/app.py
@@ -1,10 +1,10 @@
-from task_center.core.appinfo import AppInfo
-from task_center.core.sources.sources import Sources
-from task_center.core.settings.settings import Settings
+from task_center.core.appinfo import AppInfoManager
+from task_center.core.tasks import TasksManager
+from task_center.core.settings import SettingsManager
 
 
 class TaskCenterCore:
     def __init__(self):
-        self.app_info = AppInfo()
-        self.settings = Settings(self.app_info)
-        self.sources = Sources(self.settings)
+        self.app_info_manager = AppInfoManager()
+        self.settings_manager = SettingsManager(self.app_info_manager)
+        self.tasks_manager = TasksManager(self.settings_manager)
